ba5b04c14685784f47d5872295f7e3b3c5721bcb
Won Kim <wonsae.kim@gmail.com>
2019-04-05 19:39:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)
   # Your code here
+  arr.each_with_index do |d, i|
+    puts "#{i + 1}. #{d}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map! do |e|
+    e.capitalize!
+    e += "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.each do |w|
+    if w.length > 4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each do |e| 
+    cheese_types.each do |c|
+      if e == c
+        return e
+      end
+    end
+  end
+  nil
 end
