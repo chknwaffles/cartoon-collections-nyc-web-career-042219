27201059c9bdef5a8483454494d4b3371ee1ae36
Won Kim <wonsae.kim@gmail.com>
2019-04-05 19:00:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,12 @@ def long_planeteer_calls(arr)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each do |e|
+    if e.include?(cheese_types)
+      return e
+    end
+  end
 end
